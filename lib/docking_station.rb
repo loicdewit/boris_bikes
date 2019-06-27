5012b7d1d85b70cb268c3ffe5df9105cf580f8bd
Loïc de Wit <dew.loic@gmail.com>
2019-06-27 15:35:32 +0100

    Step 16 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,33 @@
 class DockingStation
 
-    attr_reader :bike
+  DEFAULT_CAPACITY = 20
+  attr_reader :bike
 
-    def release_bike
-        fail 'No bikes available' unless @bike
-        @bike
-    end
+  def initialize
+    @bike_array = []
+  end
 
-    def dock_bike(bike)
-        fail 'bike rack full' if @bike != nil
-        @bike = bike
-    end
+  def capacity
+    return DEFAULT_CAPACITY
+  end
 
-    def bike
-        @bike
-    end
+  def release_bike
+    fail 'No bikes available' if empty?
+    @bike_array.pop
+  end
+
+  def dock_bike(bike)
+    fail 'bike rack full' if full?
+    @bike_array.push(bike)
+  end
+
+  private
+
+  def full?
+    return @bike_array.length == DEFAULT_CAPACITY
+  end
+
+  def empty?
+    return @bike_array == []
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,41 +2,22 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-    it 'responds to release_bike' do
-        expect(subject).to respond_to :release_bike
-    end
+  it 'responds to release_bike' do
+    expect(subject).to respond_to :release_bike
+  end
 
-    describe '#release_bike' do
-        it 'releases bike' do
-            expect { subject.release_bike }.to raise_error("No bikes available")
-        end
+  describe '#release_bike' do
+    it 'releases bike' do
+      expect { subject.release_bike }.to raise_error("No bikes available")
     end
-    describe "#dock_bike" do
-      it "docks bike if not full" do
-        dockingstation = DockingStation.new
+  end
+  describe "#dock_bike" do
+    it "docks bike if not full" do
+      dockingstation = DockingStation.new
+      dockingstation.capacity.times do
         dockingstation.dock_bike(Bike.new)
-        expect { dockingstation.dock_bike(Bike.new)}.to raise_error("bike rack full")
       end
+      expect { dockingstation.dock_bike(Bike.new)}.to raise_error("bike rack full")
     end
-#    it 'docks bike' do
-#        expect(subject).to respond_to(:dock_bike).with(1).argument
-#    end
-
-    it 'checks if the there is a bike' do
-        expect(subject).to respond_to :bike
-    end
-
-#    it 'docks something' do
-#        bike = Bike.new
-#        expect(subject.dock_bike(bike)).to eq bike
-#    end
-
-#    it 'returns docked bike' do
-#        bike = Bike.new
-#        subject.dock_bike(bike)
-#        expect(subject.bike).to eq bike
-#    end
-
-
-
+  end
 end
